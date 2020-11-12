d481cdde1ddfc3d585c768b9d3fee9904cbb8294
Ashley Collins <ashleyjohnroycollins@gmail.com>
2020-11-12 16:57:29 +0100

    2ndtask

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+
+Cocktail.create(name: 'Mojito')
+Cocktail.create(name: 'Bloody Mary')
+Cocktail.create(name: 'Sex on the Beach')
+Cocktail.create(name: 'Margarita')
+Cocktail.create(name: 'Pina Colada')
+
+Dose.create(description: '6 leaves')
+Dose.create(description: '1.5 oz')
+Dose.create(description: '4.5 cl')
+Dose.create(description: '9 cl')
+Dose.create(description: '1.33 oz')
+Dose.create(description: '2 cl')
+
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'light rum')
+Ingredient.create(name: 'cognac')
+Ingredient.create(name: 'Irish cream')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'ginger')
+Ingredient.create(name: 'mint')
+Ingredient.create(name: 'vodka')
+Ingredient.create(name: 'rhum')
+Ingredient.create(name: 'tequila')
+Ingredient.create(name: 'apple juice')
+Ingredient.create(name: 'pinapple juice')
